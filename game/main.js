41dab9c0bdda817bd05799b63d4ac55796caa50c
OrJaku <51891265+OrJaku@users.noreply.github.com>
2021-03-06 20:30:15 +0100

    Game in progess

--- game/main.js
+++ game/main.js
@@ -1,27 +1,33 @@
-var imgs = document.getElementsByClassName("img")
+var divWidth = 210;
+var divHeight = 210;
+var marginImg = 5;
 
-for (img of imgs){
-    console.log(img.classList.contains('img_player'))
-    img.style.margin = "50px 50px 50px 50px"
-    // var n = false
-    // var k = false
-    // var p = false
+var oneLineDivSize = document.querySelectorAll('.one_line');
+for(let i=0; i<oneLineDivSize.length; i++){
+    oneLineDivSize[i].style.width = divWidth + "px";
+    oneLineDivSize[i].style.height = divHeight + "px";
+    oneLineDivSize[i].style.margin = "50px 50px";
+}
+
+var imageAlign = document.querySelectorAll('.img');
+for(let i=0; i<imageAlign.length; i++){
+    console.log(i)
+    imageAlign[i].style.margin = `${marginImg}px ${marginImg}px`
+}
+
+var imgs = document.getElementsByClassName("one_line")
+for (let img of imgs){
+    $(img).hover(
+        function () {
+            if (!(img.classList.contains("img_player"))){
+            $(this).addClass("img_hover");
+        }},
+        function () {
+            $(this).removeClass("img_hover"); 
+        });
 
     $(img).click(
         function () {
-            $(this).addClass("img_player");
+            $(this).toggleClass("img_player");
         });
-
-    if (!(img.classList.contains('img_player'))){
-    
-    $(img).hover(
-        function () {
-            $(this).addClass("img_hover");
-        },
-        function () {
-            $(this).removeClass("img_hover");
-        }
-        );
     }
-}
-
