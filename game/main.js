d85e1fafcf71f5a70daeb983091e27f9077f03ea
OrJaku <51891265+OrJaku@users.noreply.github.com>
2021-03-06 21:42:03 +0100

    Game in progess

--- game/main.js
+++ game/main.js
@@ -11,23 +11,38 @@ for(let i=0; i<oneLineDivSize.length; i++){
 
 var imageAlign = document.querySelectorAll('.img');
 for(let i=0; i<imageAlign.length; i++){
-    console.log(i)
     imageAlign[i].style.margin = `${marginImg}px ${marginImg}px`
 }
 
 var imgs = document.getElementsByClassName("one_line")
-for (let img of imgs){
-    $(img).hover(
-        function () {
-            if (!(img.classList.contains("img_player"))){
-            $(this).addClass("img_hover");
-        }},
-        function () {
-            $(this).removeClass("img_hover"); 
-        });
+    for (let img of imgs){
 
-    $(img).click(
-        function () {
-            $(this).toggleClass("img_player");
-        });
+        $(img).hover(
+            function () {
+                if (!(img.classList.contains("img_player"))){
+                    $(this).addClass("img_hover");
+            }},
+            function () {
+                $(this).removeClass("img_hover"); 
+            });
+
+        $(img).click(
+            function () {
+                $(this).toggleClass("img_player");
+                if (img.classList.contains("img_player")){
+                    console.log(`Player, ${img.id}`)
+                    computerSelect()
+
+                }
+            });
+        }
+
+    const computerSelect = function(){
+        const rnd = Math.floor(Math.random() * imgs.length);
+        var computerImage = imgs[rnd];
+        console.log("Computer", computerImage.id)
+        $(computerImage).toggleClass("img_computer");
+        setInterval(function() {
+            computerImage.style.backgroundColor = (computerImage.style.backgroundColor == 'blue' ? 'rgba(82, 82, 82, 0.1' : 'blue');
+        }, 500);
     }
