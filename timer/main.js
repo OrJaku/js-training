30fc7a8767f83b049c3779f613e157858e78a6c8
OrJaku <kuba.orzel@hotmail.com>
2021-03-07 20:43:57 +0100

    Timer in progress

--- timer/main.js
+++ timer/main.js
@@ -2,11 +2,19 @@ clockDiv = document.getElementById("clock")
 
 const clock = () => {
     var date = new Date();
-    console.log(date)
-    console.log("---------")
     hours = date.getHours();
     minutes = date.getMinutes();
     seconds = date.getSeconds();
-    clockDiv.innerHTML = `${hours}:${minutes}:${seconds}`
+    valueList = [hours, minutes, seconds]
+    strValueList = [hours, minutes, seconds]
+
+    for (let i=0; i<valueList.length; i++ ){
+        let toStr = valueList[i].toString()
+        if (toStr.length == 1){
+            toStr = `0${toStr}`
+        }
+        strValueList[i] = toStr
+    }
+    clockDiv.innerHTML = `${strValueList[0]}:${strValueList[1]}:${strValueList[2]}`
 }
 setInterval(clock, 1000)
