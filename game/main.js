0931cd55bf7164d8f82fa5528e5994fe2d419918
OrJaku <kuba.orzel@hotmail.com>
2021-03-07 11:57:40 +0100

    Game in progress

--- game/main.js
+++ game/main.js
@@ -1,6 +1,11 @@
 var divWidth = 210;
 var divHeight = 210;
 var marginImg = 5;
+var playerPoints = 0;
+var computerPoints = 0;
+var playerChoose;
+var computerChoose;
+
 
 var oneLineDivSize = document.querySelectorAll('.one_line');
 for(let i=0; i<oneLineDivSize.length; i++){
@@ -8,6 +13,8 @@ for(let i=0; i<oneLineDivSize.length; i++){
     oneLineDivSize[i].style.height = divHeight + "px";
     oneLineDivSize[i].style.margin = "50px 50px";
 }
+var score = document.getElementById("score");result
+var result = document.getElementById("result");
 
 var imageAlign = document.querySelectorAll('.img');
 for(let i=0; i<imageAlign.length; i++){
@@ -28,9 +35,10 @@ var imgs = document.getElementsByClassName("one_line")
 
         $(img).click(
             function () {
+                playerChoose = img.id;
                 $(this).toggleClass("img_player");
                 if (img.classList.contains("img_player")){
-                    console.log(`Player, ${img.id}`)
+                    console.log(`Player, ${playerChoose}`)
                     computerSelect()
 
                 }
@@ -40,9 +48,48 @@ var imgs = document.getElementsByClassName("one_line")
     const computerSelect = function(){
         const rnd = Math.floor(Math.random() * imgs.length);
         var computerImage = imgs[rnd];
-        console.log("Computer", computerImage.id)
+        computerChoose = computerImage.id
+        console.log("Computer", computerChoose)
         $(computerImage).toggleClass("img_computer");
         setInterval(function() {
             computerImage.style.backgroundColor = (computerImage.style.backgroundColor == 'blue' ? 'rgba(82, 82, 82, 0.1' : 'blue');
         }, 500);
+        if (playerChoose == computerChoose){
+            result.innerHTML = ("Remis");
+        }
+        else if (playerChoose == "nozyce" && computerChoose == "papier"){
+            result.innerHTML = ("Gracz Wygrywa");
+            playerPoints++;
+
+        }
+        else if (playerChoose == "nozyce" && computerChoose == "kamien"){
+            result.innerHTML = ("Komputer Wygrywa");
+            computerPoints++;
+
+        }
+        else if (playerChoose == "kamien" && computerChoose == "nozyce"){
+            result.innerHTML = ("Gracz Wygrywa");
+            playerPoints++;
+
+        }
+        else if (playerChoose == "kamien" && computerChoose == "papier"){
+            result.innerHTML = ("Komputer Wygrywa");
+            computerPoints++;
+
+        }
+        else if (playerChoose == "papier" && computerChoose == "kamien"){
+            result.innerHTML = ("Gracz Wygrywa");
+            playerPoints++;
+
+        }
+        else if (playerChoose == "papier" && computerChoose == "nozyce"){
+            result.innerHTML = ("Komputer Wygrywa");
+            computerPoints++;
+
+        }
+        score.innerHTML = (`Player: ${playerPoints} || Computer: ${computerPoints}`);
+
+
     }
+
+
